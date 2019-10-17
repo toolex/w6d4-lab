386a50c85aa44f2cdf377ff796b56c61e43b837b
Iain Tooley <iaintooley1@gmail.com>
2019-10-17 14:38:05 +0100

    added wat to complete single submition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+  const form = document.querySelector("#new-item-form");
+  form.addEventListener("submit", handleFormSubmit);
 })
+
+
+
+const handleFormSubmit = function (event) {
+  event.preventDefault()
+  const resultParagraph = document.querySelector("#reading-list");
+  resultParagraph.textContent = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`
+};
