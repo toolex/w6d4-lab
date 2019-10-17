34156624e0f59e5d6214e6efc845cbe7a22cf012
Iain Tooley <iaintooley1@gmail.com>
2019-10-17 15:30:57 +0100

    completed first extension delete button

--- js/app.js
+++ js/app.js
@@ -2,14 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
   const form = document.querySelector("#new-item-form");
   form.addEventListener("submit", handleFormSubmit);
+  const deleteButton = document.querySelector('#delete-button');
+  deleteButton.addEventListener('click', handleDeleteButtonClick);
 })
 
 const handleFormSubmit = function (event) {
   event.preventDefault()
-
   const newItem = document.createElement("p");
   newItem.textContent = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`;
   const resultParagraph = document.querySelector("#reading-list");
   resultParagraph.appendChild(newItem);
   this.reset();
+};
+
+const handleDeleteButtonClick = function(){
+  const resultParagraph = document.querySelector("#reading-list");
+  resultParagraph.textContent = null;
 };
